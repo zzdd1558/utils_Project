7c2169229f0466c632bec3002afda54a0c6ad764
yunjin choi <zzdd1558@naver.com>
2018-10-29 20:34:09 +0900

    DateFormat.js 를 표현할 pug파일과 Router 추가 . ( 추후에 추가할 예정 )

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/date' , (req , res , next) => {
+
+    res.render('date' ,{ title : 'Date Utils 관련 페이지' });
+})
 
 module.exports = router;
