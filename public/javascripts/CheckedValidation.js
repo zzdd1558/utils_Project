4f47f435ec069dfcbb9fbd69a91cc841aae3b81a
yunjin choi <zzdd1558@naver.com>
2018-11-03 15:49:28 +0900

    login validation check 관련 클래스 .js

---
+++ public/javascripts/CheckedValidation.js
@@ -0,0 +1,21 @@
+
+
+// 정규식 사용
+/**
+ *
+ * id :
+ * password :
+ * email :
+ * phoneNumber
+ * 
+ * */
+class CheckedValidation {
+
+    constructor(){
+
+    }
+}
+
+
+
+checkedValidation = new CheckedValidation();
