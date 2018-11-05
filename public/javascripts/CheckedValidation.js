2c7bedf9d8198ae8d0aaa1d258d07f358474970a
yunjin choi <zzdd1558@naver.com>
2018-11-05 19:05:28 +0900

    휴대폰번호 관련 정규식 추가중 . validate 로 함수 공통 묶음 추가 .

--- public/javascripts/CheckedValidation.js
+++ public/javascripts/CheckedValidation.js
@@ -1,10 +1,10 @@
 // 정규식 사용
 /**
  *
- * id :
- * password :
- * email :
- * phoneNumber :
+ * id : @김윤권
+ * password : @김윤권
+ * email : @배준
+ * phoneNumber : @최윤진
  *
  * */
 class CheckedValidation {
@@ -14,7 +14,31 @@ class CheckedValidation {
             id: /^[0-9]/,
             password: "",
             email: "",
-            phoneNum: ""
         }
     }
+
+
+    phoneValidationTest(value){
+
+        let phoneRegex = /^01[0|1|6|7|8|9]{1}/
+
+        return this.validate(value , phoneRegex)
+    }
+
+    validate ( value , regex ) {
+
+        return regex.test(value) && this.isSet(value)
+    }
+
+    isSet ( value ) {
+        return !this.isUndefined(value) && (value.length > 0 || value > 0);
+    }
+
+    isUndefined ( value ) {
+        return typeof value === "undefined";
+    }
+
 }
+
+let valid = new CheckedValidation()
+console.log(valid.phoneValidationTest("010"));
--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+
+    res.render('index', {title: 'Express'});
 });
 
-router.get('/date' , (req , res , next) => {
+router.get('/date', (req, res, next) => {
 
-    res.render('date' ,{ title : 'Date Utils 관련 페이지' });
+    res.render('date', {title: 'Date Utils 관련 페이지'});
 })
 
 module.exports = router;
