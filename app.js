ba38ecf95238b9a2606130c91e982b04541da80a
yunjin choi <zzdd1558@naver.com>
2018-11-07 09:00:58 +0900

    미들웨어 테스트중.

--- app.js
+++ app.js
@@ -13,6 +13,13 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// 미들웨어 테스트
+app.use((req , res , next) => {
+
+    console.log(`${req.url} : 미들웨어 테스트중.`);
+    next();
+});
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
