2e01ad107770a8ff920658883e60594ff6d25655
yunjin choi <zzdd1558@naver.com>
2018-11-13 01:07:44 +0900

    sequelize 모듈을 이용하여 init을 하였을 경우생성 sequelize-cli 다운 global 설치

---
+++ models/index.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const Sequelize = require('sequelize');
+
+
+const env = process.env.NODE_ENV || 'development';
+const config = require('../config/config.json')[env];
+
+const sequelize = new Sequelize(config.database , config.username  , config.password  , config);
+
+const db = {}
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.User = require("./user")(sequelize , Sequelize);
+db.Comment = require("./comment")(sequelize , Sequelize);
+
+module.export = db;
