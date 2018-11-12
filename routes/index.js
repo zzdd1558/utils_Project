246a22cd33c314706fa320ff7d114b626ebfca65
yunjin choi <zzdd1558@naver.com>
2018-11-13 00:02:25 +0900

    DB 연동을 위한 mysql2 , 보다 편한사용을 위한 ORM sequelize 설치.

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,18 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-
     res.render('index', {title: 'Express'});
 });
 
 router.get('/date', (req, res, next) => {
 
     res.render('date', {title: 'Date Utils 관련 페이지'});
-})
+});
+
+router.get('/db' , (req , res , next)  => {
+    res.render('index' , {title:'MySQL Test'});
+});
 
 module.exports = router;
